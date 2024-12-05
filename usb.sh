9d79237ecc17f403522b65a5067ed6ed61f8b925
korbykob <korbykobs@gmail.com>
2024-12-05 20:14:26 +0000

    Added folders

--- usb.sh
+++ usb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 sudo mount /dev/sdc1 /mnt/iso
-sudo cp main.efi /mnt/iso/EFI/Boot/bootx64.efi
-sudo cp font.psf /mnt/iso/font.psf
+sudo cp bin/main.efi /mnt/iso/EFI/Boot/bootx64.efi
+sudo cp src/font.psf /mnt/iso/font.psf
 sudo umount /dev/sdc1

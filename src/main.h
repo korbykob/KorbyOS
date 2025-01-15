6a0418599d03b3efd57079a3ee904d7620525258
korbykob <korbykobs@gmail.com>
2025-01-15 17:13:23 +0000

    Fix mouse driver

--- src/main.h
+++ src/main.h
@@ -427,6 +427,14 @@ void completed()
     outb(0x40, (divisor >> 8) & 0xFF);
     installInterrupt(0, pit, TRUE);
     installInterrupt(1, keyboard, TRUE);
+    outb(0x64, 0xA8);
+    outb(0x64, 0x20);
+    uint8_t status = inb(0x60) | 2;
+    outb(0x64, 0x60);
+    outb(0x60, status);
+    outb(0x64, 0xD4);
+    outb(0x60, 0xF6);
+    inb(0x60);
     outb(0x64, 0xD4);
     outb(0x60, 0xF4);
     inb(0x60);

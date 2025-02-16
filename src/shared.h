21d83f0b6f618fa5baeb049654f2f85fb1fc9829
korbykob <korbykobs@gmail.com>
2025-02-16 00:02:16 +0000

    WIP 3D game

--- src/shared.h
+++ src/shared.h
@@ -237,3 +237,45 @@ void drawString(const CHAR16* string, uint32_t x, uint32_t y, EFI_GRAPHICS_OUTPU
         address -= graphicsPitch * 32 - 16;
     }
 }
+
+const double PI=3.1415926535897932384650288;
+
+double sin(double x){
+  double sign=1;
+  if (x<0){
+    sign=-1.0;
+    x=-x;
+  }
+  if (x>360) x -= (int)(x/360)*360;
+  x*=PI/180.0;
+  double res=0;
+  double term=x;
+  int k=1;
+  while (res+term!=res){
+    res+=term;
+    k+=2;
+    term*=-x*x/k/(k-1);
+  }
+
+  return sign*res;
+}
+
+double cos(double x){
+  if (x<0) x=-x;
+  if (x>360) x -= (int)(x/360)*360;
+  x*=PI/180.0;
+  double res=0;
+  double term=1;
+  int k=0;
+  while (res+term!=res){
+    res+=term;
+    k+=2;
+    term*=-x*x/k/(k-1);
+  }  
+  return res;
+}
+
+#define min(a,b) (((a)<(b))?(a):(b))
+#define max(a,b) (((a)>(b))?(a):(b))
+
+

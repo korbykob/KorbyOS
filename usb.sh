9d38ec9e6783c5b3d9aa084beef120b8ddd96f6f
korbykob <korbykobs@gmail.com>
2025-03-06 21:27:24 +0000

    Improve usb script

--- usb.sh
+++ usb.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -e
-sudo mount /dev/sdc1 /mnt/iso
-sudo mkdir -p /mnt/iso/efi/boot
-sudo cp bin/main.efi /mnt/iso/efi/boot/bootx64.efi
-sudo mkdir -p /mnt/iso/system
-sudo cp bin/system/smp.bin /mnt/iso/system/smp.bin
-sudo mkdir -p /mnt/iso/wallpapers
-sudo cp src/wallpapers/wallpaper.bmp /mnt/iso/wallpapers/wallpaper.bmp
-sudo mkdir -p /mnt/iso/fonts
-sudo cp src/fonts/font.psf /mnt/iso/fonts/font.psf
-sudo mkdir -p /mnt/iso/programs/rendering
-sudo cp src/programs/rendering/program.bmp /mnt/iso/programs/rendering/program.bmp
-sudo cp bin/programs/rendering/program.bin /mnt/iso/programs/rendering/program.bin
-sudo cp src/programs/rendering/wall.bmp /mnt/iso/programs/rendering/wall.bmp
-sudo umount /dev/sdc1
+sudo mkdir -p staging
+sudo mount /dev/sdc1 staging
+sudo mkdir -p staging/efi/boot
+sudo cp bin/main.efi staging/efi/boot/bootx64.efi
+sudo mkdir -p staging/system
+sudo cp bin/system/smp.bin staging/system/smp.bin
+sudo mkdir -p staging/wallpapers
+sudo cp src/wallpapers/wallpaper.bmp staging/wallpapers/wallpaper.bmp
+sudo mkdir -p staging/fonts
+sudo cp src/fonts/font.psf staging/fonts/font.psf
+sudo mkdir -p staging/programs/rendering
+sudo cp src/programs/rendering/program.bmp staging/programs/rendering/program.bmp
+sudo cp bin/programs/rendering/program.bin staging/programs/rendering/program.bin
+sudo cp src/programs/rendering/wall.bmp staging/programs/rendering/wall.bmp
+sudo umount staging
+sudo rm -r staging
